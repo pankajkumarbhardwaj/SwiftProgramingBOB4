b7b71cb83044d1173a170a7afc272489431d87f2
Pankaj <Pankajp802@gmail.com>
2020-06-09 15:54:58 +0530

    Update Init Code

--- SwiftProgramingBOB/swiftPlayground/3 - Object Orianted Swift /Failable Init.playground/Contents.swift
+++ SwiftProgramingBOB/swiftPlayground/3 - Object Orianted Swift /Failable Init.playground/Contents.swift
@@ -26,3 +26,36 @@ class Course {
 let myCourse = Course(name: "MyCourse")
 let blankCourse = Course(name: "")
 
+
+//: ## required init
+class classA {
+    required init() {
+        let a = 10
+        print(a)
+    }
+}
+
+class classB: classA {
+    required init() {
+    let b = 30
+    print(b)
+    }
+}
+
+
+//______________________
+let objA = classA()
+let objB = classB()
+
+class classC: classA {
+}
+let objC = classC()
+// prints 10 ..superclass init method gets called.
+
+//: Write the required modifier before the definition of a class initializer to indicate that every subclass of the class must implement that initializer.
+
+//: You must also write the required modifier before every subclass implementation of a required initializer, to indicate that the initializer requirement applies to further subclasses in the chain. You do not write the override modifier when overriding a required designated initializer.
+
+
+//: ## Swift — Creating a Custom View From a XIB (Updated for Swift 5)
+// https://medium.com/better-programming/swift-3-creating-a-custom-view-from-a-xib-ecdfe5b3a960
--- SwiftProgramingBOB/swiftPlayground/Test File.playground/Contents.swift
+++ SwiftProgramingBOB/swiftPlayground/Test File.playground/Contents.swift
@@ -1,3 +1,8 @@
 import UIKit
 
 
+let myInt:Int = 2147483647 * 4
+
+let newInt = UInt8.max
+
+print(newInt)
